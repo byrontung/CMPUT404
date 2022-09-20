b8fca19d74a28b3ea58b211ac0ad96f58d9869b4
byrontung <btung1@ualberta.ca>
2022-09-20 09:44:47 -0600

    question 10 edit

--- lab1/version.py
+++ lab1/version.py
@@ -1,10 +1,11 @@
-from urllib.robotparser import RequestRate
 import requests
 
 def main():
     # print(requests.__version__)
-    homepage = requests.get("http://www.google.com/")
-    print(homepage)
+    # homepage = requests.get("http://www.google.com/")
+    # https://github.com/byrontung/CMPUT404/blob/main/lab1/version.py
+    content = requests.get("https://github.com/byrontung/CMPUT404/blob/main/lab1/version.py")
+    print(content)
 
 if __name__ == "__main__":
     main()
